fb895fd5c12e7a21824416fddcbc53ea27041a19
moext <dev@moext.org>
2017-09-29 04:26:09 +0000

    add redis service

--- app/Services/Redis.php
+++ app/Services/Redis.php
@@ -1,2 +1,27 @@
 <?php
 
+namespace App\Services;
+
+use Predis\Client;
+
+class Redis
+{
+    private static $instance = null;
+
+    final private function __construct(){}
+    final private function __clone(){}
+
+    public static function getInstance()
+    {
+        if (null === self::$instance)
+        {
+            self::$instance = new Client([
+                'scheme' => env('redis_scheme', 'tcp:'),
+                'host'   => env('redis_host', '127.0.0.1'),
+                'port'   => env('redis_port', 6379),
+            ]);
+        }
+
+        return self::$instance;
+    }
+}
