0a15adba975002983ab81a7ec8b8cdbacfb2b6e7
moext <dev@moext.org>
2017-09-29 02:27:28 +0000

    add db service

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Services\DB;
+
 class TestController
 {
 	public function test()
 	{
+		var_dump(DB::getInstance());
+
 		echo '<h1 style="text-align: center; margin-top: 200px">';
         echo 'Micro Framework';
         echo '</h1>';
--- app/Services/DB.php
+++ app/Services/DB.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace App\Services;
+
+use Medoo\Medoo;
+
+class DB
+{
+    private static $instance = null;
+
+    final private function __construct(){}
+    final private function __clone(){}
+
+    public static function getInstance()
+    {
+        if (null == self::$instance)
+        {
+            self::$instance = new Medoo(
+                [
+                    'database_type' => env('db_type', 'mysql'),
+                    'database_name' => env('db_name'),
+                    'server'        => env('db_server'),
+                    'username'      => env('db_username'),
+                    'password'      => env('db_password'),
+                    'charset'       => env('db_charset', 'utf8'),
+                    'port'          => env('db_port', 3306),
+                    'prefix'        => env('db_prefix', ''),
+                ]
+            );
+        }
+
+        return self::$instance;
+    }
+}
