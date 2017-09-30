34d42e6e6f4cde616c71227199753307bc38a6d7
moext <dev@moext.org>
2017-09-30 02:31:47 +0000

    add test example

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -3,13 +3,17 @@
 namespace App\Controllers;
 
 use App\Services\DB;
+use App\Services\Logger;
 
 class TestController
 {
 	public function test()
 	{
-		var_dump(DB::getInstance());
+		$db = DB::getInstance();
+		$users = $db->select('users', ['username']);
 
+		Logger::add('name', $users);
+		
 		echo '<h1 style="text-align: center; margin-top: 200px">';
         echo 'Micro Framework';
         echo '</h1>';
