e712b4f09ea90db96ee5ccf5c1b5e81b2ea15433
moext <dev@moext.org>
2017-09-30 02:05:22 +0000

    add logger service

--- app/Services/Logger.php
+++ app/Services/Logger.php
@@ -1,2 +1,19 @@
 <?php
 
+namespace App\Services;
+
+use Monolog\Logger as Log;
+use Monolog\Handler\StreamHandler;
+
+class Logger
+{
+    public static function add($name, $data, $level = Log::INFO)
+    {
+        $path = ROOT_PATH . '/log/';
+
+        $log = new Log($name);
+        $log->pushHandler(new StreamHandler($path . $name .'.'. date('Y-m-d') . '.log', $level));
+
+        $log->info($name, $data);
+    }
+}
