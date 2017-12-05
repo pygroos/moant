50985d2a2c1b49952d5696aa48eeac5365789c0d
moext <niucunhui@gmail.com>
2017-12-05 07:30:17 +0000

    load env configuration file and keys but not $_SERVER

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,9 +3,15 @@
 //  Register the auto loader
 require '../vendor/autoload.php';
 
-//  Load `.env` configuration file
+//  Load `.env` configuration file and keys but not $_SERVER
+$previousKeys = array_keys($_ENV);
 $env = new Dotenv\Dotenv(ROOT_PATH);
 $env->load();
+$currentKeys = array_keys($_ENV);
+$newKeys = array_diff($currentKeys, $previousKeys);
+array_map(function($key) {
+    unset($_SERVER[$key]);
+}, $newKeys);
 
 //  Set timezone
 @ date_default_timezone_set(env('timezone', 'UTC'));
