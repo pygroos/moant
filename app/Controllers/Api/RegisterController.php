b8422a57c5d791292703d14fc98f04bf0972d3ff
moext <niucunhui@gmail.com>
2017-12-20 02:18:04 +0000

    delete swagger document

--- app/Controllers/Api/RegisterController.php
+++ app/Controllers/Api/RegisterController.php
@@ -5,48 +5,8 @@ namespace App\Controllers;
 use App\Models\Register_V1_0;
 use App\Models\Register_V1_1;
 
-// ./vendor/bin/swagger app/Controllers/Api/ -o document/v1.0
-/**
- * @SWG\Info(
- *   version="1.0",
- *   title="Example of using references in swagger-php",
- * )
- */
 class RegisterController extends Controller
 {
-    /**
-     * @SWG\Post(
-     *     path="/users",
-     *     summary="用户注册",
-     *     @SWG\Parameter(
-     *          name="email, username, password",
-     *          in="body",
-     *          required=true,
-     *          @SWG\Schema(
-     *              @SWG\Property(
-     *                  property="email",
-     *                  type="string",
-     *                  maximum=64
-     *              ),
-     *              @SWG\Property(
-     *                  property="username",
-     *                  type="string",
-     *                  maximum=12
-     *              ),
-     *              @SWG\Property(
-     *                  property="password",
-     *                  type="string",
-     *                  maximum=64
-     *              )
-     *          )
-     *     ),
-     *     @SWG\Response(
-     *          response=200,
-     *          description="注册成功",
-     *     ),
-     *     security={{"Bearer":{}}}
-     * )
-     */
     public function action()
     {
         $email = $this->request->getParam('email', '');
